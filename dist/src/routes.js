d77523c52215668f3ab978acf6bfaeb7464056ca
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2022-03-22 20:27:34 +0000

    Heroku

--- dist/src/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const projects_1 = require("./controllers/projects");
-function app(app) {
-    app.get('/', projects_1.getAllProjectsHandler);
-}
-exports.default = app;
